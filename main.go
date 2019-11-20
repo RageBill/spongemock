405f890f0075b638b894eb767478cc047b8296fc
RageBill <chubillkelvin@gmail.com>
2019-11-21 00:03:17 +0800

    Rename body field to input and output, basic post functionality working.

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"github.com/gorilla/mux"
+	"strings"
 )
+
+type Message struct {
+	Input  string `json:"input"`
+	Output string `json:"output"`
+}
 
 func get(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -13,9 +22,37 @@ func get(w http.ResponseWriter, r *http.Request) {
 }
 
 func post(w http.ResponseWriter, r *http.Request) {
+	// set http header
 	w.Header().Set("Content-Type", "application/json")
+
+	// read request body from request
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// parse body to json
+	var content Message
+	err = json.Unmarshal(body, &content)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// prepare response message
+	var response = Message{Input: content.Input, Output: ""}
+	temp := strings.Split(content.Input, " ")
+	for _, word := range temp {
+		for _, char := range strings.Split(word, "") {
+			response.Output += char
+			fmt.Println(response.Output)
+		}
+	}
+
+	// write to http response
 	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(`{"message": "post called"}`))
+	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
